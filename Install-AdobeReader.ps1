c01edb6b5753f1edccb86797fe90a96b555fb80b
Zach <zach.a.choate@gmail.com>
2020-06-14 23:16:58 -0400

    Adding updates for PS context. (#13)

--- Install-AdobeReader.ps1
+++ Install-AdobeReader.ps1
@@ -5,6 +5,27 @@ param(
 $Mode
 )
 
+$argsString = ""
+If ($ENV:PROCESSOR_ARCHITEW6432 -eq "AMD64") {
+    Try {
+        foreach($k in $MyInvocation.BoundParameters.keys)
+        {
+            switch($MyInvocation.BoundParameters[$k].GetType().Name)
+            {
+                "SwitchParameter" {if($MyInvocation.BoundParameters[$k].IsPresent) { $argsString += "-$k " } }
+                "String"          { $argsString += "-$k `"$($MyInvocation.BoundParameters[$k])`" " }
+                "Int32"           { $argsString += "-$k $($MyInvocation.BoundParameters[$k]) " }
+                "Boolean"         { $argsString += "-$k `$$($MyInvocation.BoundParameters[$k]) " }
+            }
+        }
+        Start-Process -FilePath "$ENV:WINDIR\SysNative\WindowsPowershell\v1.0\PowerShell.exe" -ArgumentList "-File `"$($PSScriptRoot)\$($MyInvocation.MyCommand)`" $($argsString)" -Wait -NoNewWindow
+    }
+    Catch {
+        Throw "Failed to start 64-bit PowerShell"
+    }
+    Exit
+}
+
 ## Install block ##
 if($Mode -eq "Install") {
     
